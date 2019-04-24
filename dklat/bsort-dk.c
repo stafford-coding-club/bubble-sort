e5e0304aefdc28d6b5fffcc35d46327cf84fc2c0
Dawid klatkiewicz <klatkiewiczdawid@gmail.com>
2019-04-23 23:48:43 -0400

    Made genRandom function

--- dklat/bsort-dk.c
+++ dklat/bsort-dk.c
@@ -9,6 +9,19 @@ void printIntArray(int* array, int size) {
    printf("]\n");
 }
 
+unsigned int genRandom(unsigned int min, unsigned int max) {
+   int r;
+   const unsigned int range   = 1 + max - min;
+   const unsigned int buckets = RAND_MAX / range;
+   const unsigned int limit   = buckets * range;
+
+   do {
+      r = rand();
+   } while (r >= limit);
+
+   return min + (r / buckets);
+}
+
 int main(int argc, char** argv) {
 
    if (argc < 2) {
@@ -18,16 +31,21 @@ int main(int argc, char** argv) {
 
    const int size = atoi(argv[1]);
 
-   int min = 0;
-   int max = 500;
+   unsigned int min = 0;
+   unsigned int max = 500;
 
    if (argc >= 3) min = atoi(argv[2]);
    if (argc >= 4) max = atoi(argv[3]);
 
+   if (min > max) {
+      printf("MIN GREATER THAN MAX\n");
+      return 0;
+   }
+
    int nums[size];
 
    for (int i = 0; i < size; i++) {
-      nums[i] = rand() % max + min;
+      nums[i] = genRandom(min,max);
    }
 
    printIntArray(nums, size);
